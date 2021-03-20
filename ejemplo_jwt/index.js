de7910f741f8a3f7fbaf06df605d2521114116f3
JosueOH <josue_pknz97@hotmail.com>
2021-03-20 00:00:36 -0600

    Ejemplo jwt 19/03/2021

---
+++ ejemplo_jwt/index.js
@@ -0,0 +1,17 @@
+var jwt = require('jsonwebtoken');
+
+var payload={
+    userdata:{'email': 'joe@doe.com',
+               'phone': '5544667788', 
+               'perfil': 'user'
+    }
+};
+const clave = "dios1234";
+
+const token= jwt.sign(payload, clave, {expiresIn: 60*5} );
+
+console.log(token);
+
+jwt.verify(token, 'wrong-secret', (err, decoded) =>{
+
+});
